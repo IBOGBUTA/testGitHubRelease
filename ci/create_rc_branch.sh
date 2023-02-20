d48bf05ca2f8d37c3bc53557522929901c0e6270
Glaucio Czekailo <glaucio.porcidesczekailo@atos.net>
2023-02-20 16:19:29 +0200

    update file

--- ci/create_rc_branch.sh
+++ ci/create_rc_branch.sh
@@ -9,7 +9,7 @@ fi
 
 # get the latest tag
 git fetch --tags
-tag=$(git describe --tags --abbrev=0)
+tag=$(git tag --sort=-v:refname | grep -E '^[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$' | head -n1)
 
 # get the major, minor, and patch numbers
 if [[ $tag =~ ([0-9]+)\.([0-9]+)\.([0-9]+)-SNAPSHOT ]]; then
