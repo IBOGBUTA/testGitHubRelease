938769ef98215898e173a43ec6160bb037c3ee9f
Bogdan Buta <bogdan.buta@atos.net>
2023-03-09 11:36:33 +0200

    [WF] optimizing create rel branch script

--- ci/common_release_functions.sh
+++ ci/common_release_functions.sh
@@ -7,3 +7,15 @@ runningOnMaster() {
 	fi
 	return 0
 }
+
+updateMavenConfig() {
+	if [ $# -ne 2 ]; then
+		echo "Error: updateMavenConfig() - Invalid number of parameters provided. Expected 2, received $#."
+		return 1
+	fi
+	version=$1
+	qualifier=$2	
+	sed -i "s/-Drevision=.*/-Drevision=$version/" .mvn/maven.config
+	sed -i "s/-Dchangelist=.*/-Dchangelist=$qualifier/" .mvn/maven.config
+	return 0
+}
--- ci/create_release_branch.sh
+++ ci/create_release_branch.sh
@@ -4,16 +4,11 @@
 ### This script can run only on master branch
 
 source common_release_functions.sh
-git checkout VERSION-3.0.0
-runningOnMaster && { echo "Called from master. Script can continue."; } || { echo "Error: You should create a RC branch from the master branch."; exit 1; }
-git checkout master
+TAG_FORMAT_ON_MASTER="^[0-9]*+\.[0-9]*+\.[0-9]*+-SNAPSHOT$"
+TAG_PATTERN_ON_MASTER="([0-9]+)\.([0-9]+)\.([0-9]+)-SNAPSHOT"
 
-# Check if the current branch name matches the pattern master
-#branch_name=$(git rev-parse --abbrev-ref HEAD)
-#if [[ ! $branch_name =~ ^master$ ]]; then
-#  echo "Error: You should create a RC branch from the master branch."
-#  exit 1
-#fi
+# This script can run only on master branch
+runningOnMaster && { echo "$0 called from master. Script can continue."; } || { echo "Error: You should create a RC branch from the master branch."; exit 1; }
 
 # Get the release type of this new branch
 # Check if the first argument exists
@@ -24,23 +19,23 @@ fi
 release_type="$1"
 
 if [[ "$release_type" == "major" || "$release_type" == "minor" || "$release_type" == "patch" ]]; then
-    echo "A new branch for a $release_type release will be created."
+    echo "$0 called with release type: $release_type. Script can continue."
 else
-    echo "Error: Incorrect release type. Possible values: major, minor, patch"
+    echo "Error: Unknown release type: $release_type. Possible values: major, minor, patch"
     exit 1
 fi
 
 # get the latest tag
 git fetch --tags
-tag=$(git tag --sort=-v:refname | grep -E '^[0-9]*+\.[0-9]*+\.[0-9]*+-SNAPSHOT$' | head -n1)
+tag=$(git tag --sort=-v:refname | grep -E $TAG_FORMAT_ON_MASTER | head -n1)
 
 # get the major, minor, and patch
-if [[ $tag =~ ([0-9]+)\.([0-9]+)\.([0-9]+)-SNAPSHOT ]]; then
+if [[ $tag =~ $TAG_PATTERN_ON_MASTER ]]; then
     major=${BASH_REMATCH[1]}
     minor=${BASH_REMATCH[2]}
     patch=${BASH_REMATCH[3]}
 else
-    echo "Error: tag ($tag) is not in the correct format" >&2
+    echo "Error: Latest tag on master ($tag) is not in the correct format." >&2
     exit 1
 fi
 
@@ -50,21 +45,12 @@ if [[ "$release_type" == "major" ]]; then
 	patch=0
 elif [[ "$release_type" == "minor" ]]; then
 	((minor++))
-	patch=0	
-else 
-	#patch release, will use the tag on the the master
-	#nothing to do here for now	
-	echo "New patch, will user version on master branch"
+	patch=0
 fi
 
-echo "New $release_type release version base will be ${major}.${minor}.${patch}"
-
+echo "New $release_type release version branch will be named VERSION-${major}.${minor}.${patch}"
 # Setup new branch name
 branch="VERSION-${major}.${minor}.${patch}"
-
-# Setup new RC tag name for initial build
-#new_rc_version="${major}.${minor}.${patch}"
-#new_rc_qualifier="-RC1"
 
 # Setup RC tag name for future development builds on this release branch
 future_rc_version="${major}.${minor}.${patch}"
@@ -76,56 +62,33 @@ new_master_version="${major}.${minor}.${master_patch}"
 new_master_qualifier="-SNAPSHOT"
 
 # create RC branch
-git checkout -b "$branch"
-echo "1. will checkout branch $branch"
+git checkout -b "$branch" >/dev/null 2>&1
 
-
-# Update the Maven version in the maven.config file
-#sed -i "s/-Drevision=.*/-Drevision=$new_rc_version/" .mvn/maven.config
-#sed -i "s/-Dchangelist=.*/-Dchangelist=$new_rc_qualifier/" .mvn/maven.config
-#echo "2. will update .mvn/maven.config on branch to $new_rc_version and $new_rc_qualifier"
-
-
-#drop the snapshot for now
-#git diff --exit-code --quiet .mvn/maven.config || git commit -m "Automatic update of version" .mvn/maven.config
-#git tag "$new_rc_version$new_rc_qualifier" "$branch"
-#echo "3. will commit the .mvn/maven.config changes and  create a tag $new_rc_version$new_rc_qualifier"  
-
-
-# Build goes here
-#
-#
-#echo "4. Build goes here"
-
-# Update the Maven version in the maven.config file for future RC builds
-sed -i "s/-Drevision=.*/-Drevision=$future_rc_version/" .mvn/maven.config
-sed -i "s/-Dchangelist=.*/-Dchangelist=$future_rc_qualifier/" .mvn/maven.config
-echo "5. will update .mvn/maven.config on branch to $future_rc_version and $future_rc_qualifier"
-
-git diff --exit-code --quiet .mvn/maven.config || git commit -m "Automatic update of version" .mvn/maven.config
-git tag "$future_rc_version$future_rc_qualifier" "$branch"
-echo "6. will commit the .mvn/maven.config changes and  create a tag $future_rc_version$future_rc_qualifier" 
-
-# push the new tags and branch to remote
-git push --set-upstream origin "$branch"
-git push --tags #not recommended
-echo "7. will push the branch $branch"
+# update Maven config on release branch
+updateMavenConfig "$future_rc_version" "$future_rc_qualifier" && 
+	{ echo "Updating .mvn/maven.config on branch $branch to $future_rc_version$future_rc_qualifier"; } || 
+	{ echo "Error: Failed to update Maven Config to $future_rc_version$future_rc_qualifier"; exit 1; }
+git diff --exit-code --quiet .mvn/maven.config || git commit -m "[WF] Automatic update of Maven version to $future_rc_version$future_rc_qualifier" .mvn/maven.config
+git tag "$future_rc_version$future_rc_qualifier" "$branch" >/dev/null 2>&1
+echo "Actions done on branch $branch: New commit for the .mvn/maven.config changes, new tag $future_rc_version$future_rc_qualifier created." 
 
 # back to master branch to continue the job.
-git checkout master
-echo "8. will move to master"
+git checkout master >/dev/null 2>&1
 
-# Update the Maven version in the maven.config file
-sed -i "s/-Drevision=.*/-Drevision=$new_master_version/" .mvn/maven.config
-sed -i "s/-Dchangelist=.*/-Dchangelist=$new_master_qualifier/" .mvn/maven.config
-echo "9. will update .mvn/maven.config on master to $new_master_version and $new_master_qualifier"
-
-# Do the commit in master branch
+# update Maven config on master
+updateMavenConfig "$new_master_version" "$new_master_qualifier" && 
+	{ echo "Updating .mvn/maven.config on master to $new_master_version$new_master_qualifier"; } || 
+	{ echo "Error: Failed to update Maven Config to $new_master_version$new_master_qualifier"; exit 1; }
 git diff --exit-code --quiet .mvn/maven.config || git commit -m "Automatic update of version" .mvn/maven.config
 git tag "$new_master_version$new_master_qualifier" master
-echo "10. will commit the .mvn/maven.config changes and  create a tag $new_master_version$new_master_qualifier" 
+echo "Actions done on master: New commit for the .mvn/maven.config changes, new tag $new_master_version$new_master_qualifier." 
 
-#git push origin master
-#git push --tags #not recommended
-#git push
-echo "11. will push changes to master"
+#git checkout "$branch" >/dev/null 2>&1
+#git push --set-upstream origin "$branch" >/dev/null 2>&1
+#git push --tags >/dev/null 2>&1
+echo "Changes on branch $branch saved"
+
+#git checkout master >/dev/null 2>&1
+#git push origin master >/dev/null 2>&1
+#git push --tags >/dev/null 2>&1
+echo "Changes on master saved"
