1920803b1fe7f1ceee31e9334dfdd0663c9b7091
Bogdan Buta <bogdan.buta@atos.net>
2023-03-09 11:52:36 +0200

    [WF] optimizing create rel branch script

--- ci/create_release_branch.sh
+++ ci/create_release_branch.sh
@@ -5,7 +5,7 @@
 
 echo "Running from path: "$(dirname "${BASH_SOURCE[0]}")
 
-source ./common_release_functions.sh
+source common_release_functions.sh || {echo "Error: Cannot reach external resource common_release_functions.sh. Will exit."; exit 1; }
 TAG_FORMAT_ON_MASTER="^[0-9]*+\.[0-9]*+\.[0-9]*+-SNAPSHOT$"
 TAG_PATTERN_ON_MASTER="([0-9]+)\.([0-9]+)\.([0-9]+)-SNAPSHOT"
 
